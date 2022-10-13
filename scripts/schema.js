023c5735ead80844708c6ef3fbbf2a8c9f0b5949
QJAutumn <780588304@qq.com>
2022-10-13 16:19:20 +0800

    [add]新增移动文件,复制文件等操作

--- scripts/schema.js
+++ scripts/schema.js
@@ -55,6 +55,58 @@ function Relation() {
 }
 
 /**
+ * 文件复制移动逻辑
+ */
+function Move() {
+  var base_dir = ".trash";
+  // 判断文件夹是否存在.不存在就创建
+  Mkdir(base_dir);
+  var new_dir = parseInt(Date.now() / 1000);
+  // models的文件移动到
+  var models = Relation();
+  for (var i in models) {
+    var target_name = "models/" + models[i]["table"]["name"] + ".mod.json";
+    // 如果表已经存在,则
+    if (Exists(models[i]["table"]["name"])) {
+      Mkdir(base_dir + "/" + new_dir);
+      Copy(
+        target_name,
+        base_dir + "/" + new_dir,
+        models[i]["table"]["name"] + ".mod.json"
+      );
+
+      // 复制完成后,删除文件
+      Process("fs.dsl.Remove", target_name);
+    } else {
+      // 否则就创建文件
+      Process("fs.dsl.WriteFile", target_name);
+    }
+  }
+}
+/**
+ * 查看模型是否存在
+ * @param {*} file_name
+ * @returns
+ */
+function Exists(file_name) {
+  file_name = file_name + ".mod.json";
+  var res = Process("fs.dsl.Exists", "models/" + file_name);
+  return res;
+}
+
+function Mkdir(name) {
+  var res = Process("fs.dsl.ReadDir", name);
+  if (res.code) {
+    Process("fs.dsl.Mkdir", name);
+  }
+}
+
+function Copy(from, to, name) {
+  console.log([from, to]);
+  Process("fs.dsl.Copy", from, to + "/" + name);
+}
+
+/**
  * 获取图标
  */
 function Icon() {}
